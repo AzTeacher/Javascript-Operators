267e00df6a366c54d5d024c890b2ef23c0434647
azteacher <az.teachers.2020@gmail.com>
2023-01-25 19:48:21 +0300

    Javascript Operators

---
+++ app.js
@@ -0,0 +1,147 @@
+          // Javascipt Operators
+          // *******************************************************
+
+          // 1. Arithmetic Operators  => Riyazi əməliyyatlar operatorları
+          //   x = 7;
+          //   y = 2;
+
+          //   z = x + y
+          //   z = x - y;
+          //   z = x * y;
+          //   z = x / 2
+          //   z = x ** y
+          //   z= x % y
+          //   z = x++;
+          //   z = ++x
+          //   z = x--;
+          //   z = --x;
+
+          // console.log(x)
+
+
+
+          // *******************************************************
+
+
+          // 2. Assignment Operators   =>  Təyinat operatorları
+
+          //   x = 2;
+          //   y = 2
+
+          //   +=
+          //   x+=y;  x+y;
+          // -= x-=y  x-y
+          //   *= x*=y  x*y
+          //   /=   x/=y  x/y
+          //   %=   x%=y  x/y = 
+
+          //   **=   x**=y  
+
+
+          //   console.log(x **= y)
+
+
+          // *******************************************************
+
+
+          // 3. Comparison Operators   =>  Müqayisə operatorları
+
+
+
+
+
+          //   z = x == y
+          //   v = x === y;
+          // z = x != y
+          //   z = x !== y
+          //   z = x > y
+          //   z = x < y
+          //   z = x >= y
+          //   z = x <= y
+
+          //   console.log(z)
+          //   console.log(v)
+
+
+
+          // *******************************************************
+
+
+          // 4. Logical Operators   =>  Məntiqi Operatorlar
+
+          //   &&
+          //   ||
+          //   !
+
+
+          //   x = 5
+          //   y = 6
+          //   z = 7
+          //   v = 8
+          //   && - and Operator 
+          //   true && false => false
+          //   false && true =>  false
+          //   true && true  => true
+          //   false && false =>false
+          //   if (x == y && z == v) {
+          //       console.log("duzdur")
+          //   } else {
+          //       console.log("yanlisdir")
+          //   }
+
+          //   || - or Operator
+          //   true && false => true
+          //   false && true => true
+          //   true && true  => true
+          //   false && false => false
+          //   if (x == y || z == v) {
+          //       console.log("duzdur")
+          //   } else {
+          //       console.log("yanlisdir")
+          //   }
+
+
+          //   ! not Operatoru
+          //   true => false
+          //   false => true
+          //   if (x !=y) {
+          //       console.log("duzdur")
+          //   } else {
+          //       console.log("yanlisdir")
+          //   }
+
+
+          // *******************************************************
+
+
+          // 5. Conditional (ternary) operator  =>  Şərti (üçlü) operator
+
+          //   a = 5
+          //   b = 6
+
+          //   if (a == b) {
+          //       console.log("Beraberdir")
+          //   } else if (a > b) {
+          //       console.log("a boyukdur b den ")
+          //   } else {
+          //       console.log("b boyukdur a dan")
+          //   }
+
+          //   x = a == b ? "Beraberdir" : a > b ? "a boyukdur b den" : "b boyukdur a dan";
+
+          //   console.log(x)
+
+
+
+
+
+          // *******************************************************
+
+          // 6. Type Operators   =>  Növ operatorları
+
+          x = 6
+          y = "6"
+
+
+          console.log(typeof x)
+          console.log(typeof y)
